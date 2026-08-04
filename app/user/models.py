25955c215ddcaef648674d81cab92fe1a83ecb96
codewithniharika01 <niharikabharti240@gmail.com>
2026-08-04 06:03:45 +0530

    Fix User model import

--- app/user/models.py
+++ app/user/models.py
@@ -2,7 +2,7 @@ from sqlalchemy import Column, Integer, String
 from app.db import Base
 
 
-class User(Base):
+class Users(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
